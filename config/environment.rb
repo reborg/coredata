ddd2ba8f32c1ad49295807a298c5103cb7fd8ea8
reborg <reborg@reborg.net>
2010-09-17 18:00:20 +0100

    persisence class and related spec passing fine, also imported necessary deps into vendor dir

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+framework 'coredata'
+APP_ROOT = File.expand_path(File.dirname(__FILE__) + "/..")
+$LOAD_PATH << APP_ROOT + "/lib/"
+
+# Get vendor libs into the loadpath:
+[ "mocha/lib", "mocha-on-bacon/lib" ].each do |path|
+  $LOAD_PATH << APP_ROOT + "/vendor/" + path
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
+require 'bacon'
+require 'mocha-on-bacon'
+
+TEST_BUNDLE_PATH = File.dirname(__FILE__) + "/test_db"
+TEST_DB_PATH = TEST_BUNDLE_PATH + "/pomodori.xml"
+
+require File.expand_path(File.dirname(__FILE__) + "/spec_commons")
+include SpecCommons
