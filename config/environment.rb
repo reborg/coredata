43baf0658f04a4cf461dafdd81fd028330506022
reborg <reborg@reborg.net>
2010-09-26 17:49:53 +0100

    also macruby of single spec files is supported now

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,6 @@ APP_ROOT = File.expand_path(File.dirname(__FILE__) + "/..")
 $LOAD_PATH << APP_ROOT + "/lib/"
 
 # Get vendor libs into the loadpath:
-[ "mocha/lib", "mocha-on-bacon/lib" ].each do |path|
+[ "bacon/lib", "mocha/lib", "mocha-on-bacon/lib" ].each do |path|
   $LOAD_PATH << APP_ROOT + "/vendor/" + path
 end
--- spec/spechelper.rb
+++ spec/spechelper.rb
@@ -2,6 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'bacon'
 require 'mocha-on-bacon'
 
+Bacon.summary_on_exit
 TEST_BUNDLE_PATH = File.dirname(__FILE__) + "/test_db"
 TEST_DB_PATH = TEST_BUNDLE_PATH + "/pomodori.xml"
 
