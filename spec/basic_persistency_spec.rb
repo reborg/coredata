07f1d105b4f863cee8bd4209add7518150cb5831
reborg <reborg@reborg.net>
2012-02-03 07:48:43 +0000

    high level basic CRUD operation acceptance spec, now commented out, but that's the goal

---
+++ spec/basic_persistency_spec.rb
@@ -0,0 +1,29 @@
+require File.dirname(__FILE__) + "/spechelper"
+
+class Book; end
+
+describe "basic CRUD operations on persistent objects" do
+  
+  before do
+    @book = Book.new
+  end
+
+  #it 'there are no persisted books at startup' do
+  #  Book.all.size.should.be == 0
+  #end
+
+  #it 'persistent objects are given an ID' do
+  #  Book.create.id.should.not.be nil 
+  #end
+
+  #it 'retrieves all persistent objects in one go' do
+  #  Book.create 
+  #  Book.create 
+  #  Book.all.size.should.be == 2
+  #end
+
+  #after do
+  #  Database.cleanup
+  #end
+
+end
